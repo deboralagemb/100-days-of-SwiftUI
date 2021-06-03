bbeb7d155b72ac908e5e412f8a99f88073046bb0
deboralagemb <deboralagemb@gmail.com>
2021-06-02 21:30:33 -0300

    add: initial project named WeSplit

---
+++ WeSplit-project1/WeSplit-project1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit-project1
+//
+//  Created by Débora Lage on 02/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit-project1/WeSplit-project1/WeSplit_project1App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_project1App.swift
+//  WeSplit-project1
+//
+//  Created by Débora Lage on 02/06/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
