8b155193ebf68cdde3e94d1b53d5d923fa7858f9
deboralagemb <deboralagemb@gmail.com>
2021-06-06 11:36:58 -0300

    add: challenge1 started

---
+++ Challenge1/Challenge1/Challenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge1App.swift
+//  Challenge1
+//
+//  Created by Débora Lage on 06/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Challenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
