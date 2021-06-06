7ae906de14cc09e7fa9a95550182069e3a9a6fcc
deboralagemb <deboralagemb@gmail.com>
2021-06-06 12:09:19 -0300

    add: layout

--- Challenge1/Challenge1/ContentView.swift
+++ Challenge1/Challenge1/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var initialValue = ""
+    @State private var initialUnit = "Celsius"
+    @State private var convertedValue = ""
+    @State private var convertedUnit = "Celsius"
+    
+    let units = ["Celsius", "Fahrenheit", "Kelvin"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Initial value", text: $initialValue)
+                    Picker("Initial unit", selection: $initialUnit) {
+                        ForEach(0 ..< units.count) {
+                            Text("\(self.units[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section {
+                    TextField("Converted value", text: $convertedValue).disabled(true)
+                    
+                    Picker("Unit to convert", selection: $convertedUnit) {
+                        ForEach(0 ..< units.count) {
+                            Text("\(self.units[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+            }
+            .navigationBarTitle("TemperatureConverter", displayMode: .large)
+        }
     }
 }
 
