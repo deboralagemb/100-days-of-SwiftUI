bea83333f87f4aa387613ac8419717885f9042b6
deboralagemb <deboralagemb@gmail.com>
2021-06-07 11:56:46 -0300

    add: gradients.

--- GuessTheFlag-project2/GuessTheFlag-project2/ContentView.swift
+++ GuessTheFlag-project2/GuessTheFlag-project2/ContentView.swift
@@ -9,10 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.red.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            Text("Your content")
-        }
+//        LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .leading, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
+//        RadialGradient(gradient: Gradient(colors: [Color.blue, Color.black]), center: .center, startRadius: 20, endRadius: /*@START_MENU_TOKEN@*/500/*@END_MENU_TOKEN@*/)
+        AngularGradient(gradient: Gradient(colors: [Color.red, Color.yellow, Color.green, Color.blue, Color.purple, Color.red]), center: .center)
     }
 }
 
