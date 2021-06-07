a93222b304f8ff279cb31250f37155d8c6aafac3
deboralagemb <deboralagemb@gmail.com>
2021-06-07 12:02:54 -0300

    add: buttons and images.

--- GuessTheFlag-project2/GuessTheFlag-project2/ContentView.swift
+++ GuessTheFlag-project2/GuessTheFlag-project2/ContentView.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .leading, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
-//        RadialGradient(gradient: Gradient(colors: [Color.blue, Color.black]), center: .center, startRadius: 20, endRadius: /*@START_MENU_TOKEN@*/500/*@END_MENU_TOKEN@*/)
-        AngularGradient(gradient: Gradient(colors: [Color.red, Color.yellow, Color.green, Color.blue, Color.purple, Color.red]), center: .center)
+        Button(action: {
+            print("button tapped")
+        }) {
+            HStack(spacing: 20) {
+                Image(systemName: "pencil")
+                Text("edit")
+            }
+        }
     }
 }
 
