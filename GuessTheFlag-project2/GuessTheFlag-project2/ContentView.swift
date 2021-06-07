9fe3a2c8af7ce491a7cba013179b7e95255f8d2b
deboralagemb <deboralagemb@gmail.com>
2021-06-07 10:48:23 -0300

    add: project2 started

---
+++ GuessTheFlag-project2/GuessTheFlag-project2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag-project2
+//
+//  Created by Débora Lage on 07/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag-project2/GuessTheFlag-project2/GuessTheFlag_project2App.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlag_project2App.swift
+//  GuessTheFlag-project2
+//
+//  Created by Débora Lage on 07/06/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlag_project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
