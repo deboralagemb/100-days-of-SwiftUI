7a24280839abcec2770b2551ef9f0274bbb9ad7b
deboralagemb <deboralagemb@gmail.com>
2021-06-09 19:54:52 -0300

    add - challenge 3 - showing alert message when wrong flag is tapped.

--- GuessTheFlag-project2/GuessTheFlag-project2/ContentView.swift
+++ GuessTheFlag-project2/GuessTheFlag-project2/ContentView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
-    @State private var showingScore = false
     @State private var totalScore = 0
-    @State private var scoreTitle = ""
+    @State private var flagTappedName = ""
+    @State private var showingWrongFlagAlert = false
     
     var body: some View {
         ZStack {
@@ -33,6 +33,7 @@ struct ContentView: View {
                 ForEach( 0 ..< 3) { number in
                     Button(action: {
                         self.flagTapped(number)
+                        self.flagTappedName = countries[number]
                     }) {
                         Image(self.countries[number])
                             .renderingMode(.original)
@@ -48,8 +49,8 @@ struct ContentView: View {
                 
                 Spacer()
             }
-            .alert(isPresented: $showingScore) {
-                Alert(title: Text(scoreTitle), message: Text("Your score is \(totalScore)"), dismissButton: .default(Text("Continue")) {
+            .alert(isPresented: $showingWrongFlagAlert) {
+                Alert(title: Text("Wrong"), message: Text("That’s the flag of \(flagTappedName)"), dismissButton: .default(Text("Continue")) {
                     self.askQuestion()
                 })
             }
@@ -59,13 +60,11 @@ struct ContentView: View {
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             totalScore += 1
-            scoreTitle = "Correct"
+            askQuestion()
         } else {
             totalScore -= 1
-            scoreTitle = "Wrong"
+            showingWrongFlagAlert = true
         }
-        
-        showingScore = true
     }
     
     func askQuestion() {
