81c6e2a4dff6caa36e5a886450bec677dbf7ac30
deboralagemb <deboralagemb@gmail.com>
2021-06-09 19:40:53 -0300

    add - challenge 1 - add an @State property to store the user’s score, modify it when they get an answer right or wrong, then display it in the alert.

--- GuessTheFlag-project2/GuessTheFlag-project2/ContentView.swift
+++ GuessTheFlag-project2/GuessTheFlag-project2/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var showingScore = false
+    @State private var totalScore = 0
     @State private var scoreTitle = ""
     
     var body: some View {
@@ -44,7 +45,7 @@ struct ContentView: View {
                 Spacer()
             }
             .alert(isPresented: $showingScore) {
-                Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+                Alert(title: Text(scoreTitle), message: Text("Your score is \(totalScore)"), dismissButton: .default(Text("Continue")) {
                     self.askQuestion()
                 })
             }
@@ -53,8 +54,10 @@ struct ContentView: View {
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
+            totalScore += 1
             scoreTitle = "Correct"
         } else {
+            totalScore -= 1
             scoreTitle = "Wrong"
         }
         
