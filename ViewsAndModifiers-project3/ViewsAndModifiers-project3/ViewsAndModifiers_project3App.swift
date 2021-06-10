6e446993fd75f774efa4f71d5a8941132551eaaa
deboralagemb <deboralagemb@gmail.com>
2021-06-10 09:51:20 -0300

    add: ViewsAndModifiers project

---
+++ ViewsAndModifiers-project3/ViewsAndModifiers-project3/ViewsAndModifiers_project3App.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiers_project3App.swift
+//  ViewsAndModifiers-project3
+//
+//  Created by Débora Lage on 10/06/21.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiers_project3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
