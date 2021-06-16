724b40d08fa840d1fc0ce8d6dbed629f70ae9c73
deboralagemb <deboralagemb@gmail.com>
2021-06-16 20:20:17 -0300

    add: dark color scheme

--- Challenge2/Challenge2/ContentView.swift
+++ Challenge2/Challenge2/ContentView.swift
@@ -9,25 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     private var moves: [String] = ["Rock", "Paper", "Scissors"]
-    @State private var shouldBeatMove: Bool = Bool.random()
+    @State private var shouldWin: Bool = Bool.random()
+    @State private var moveIndex: Int = Int.random(in: 0 ..< 3)
     
     var body: some View {
         NavigationView {
-            
-            HStack {
-                ForEach(0 ..< moves.count) { index in
-                    Button(moves[index]) {
-                        print("button \(moves[index]) tapped")
+            ZStack {
+                VStack {
+                    Text(shouldWin ? "Choose an option to beat:" : "Choose an option to be defeated by:")
+                        .frame(alignment: .center)
+                        .font(.system(size: 22))
+                        .foregroundColor(.white)
+                    
+                    Text(moves[moveIndex])
+                        .frame(alignment: .center)
+                        .font(.system(size: 22))
+
+                    HStack {
+                        ForEach(0 ..< moves.count) { index in
+                            Button(moves[index]) {
+                                print("button \(moves[index]) tapped")
+                            }
+                            .frame(width: 100, height: 100, alignment: .center)
+                            .background(Color.gray)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                        }
                     }
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .background(Color.purple)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
                 }
+                .navigationBarTitle(Text("Challenge: RPC"))
             }
-            
-            .navigationBarTitle(Text("Challenge RPC"))
         }
+        .preferredColorScheme(.dark) 
     }
     
 }
