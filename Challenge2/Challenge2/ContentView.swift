81cf6d422ede08121586e7720f169d99ed73089d
deboralagemb <deboralagemb@gmail.com>
2021-06-17 14:53:01 -0300

    add: colors corresponding to winning or losing.

--- Challenge2/Challenge2/ContentView.swift
+++ Challenge2/Challenge2/ContentView.swift
@@ -13,6 +13,7 @@ enum Result {
 
 struct ContentView: View {
     private var moves: [String] = ["Rock", "Paper", "Scissors"]
+    private var emojiHand: [String] = ["👊", "✋", "✌️"]
     @State private var shouldWin: Result = Bool.random() ? .win : .lose
     @State private var moveIndex: Int = Int.random(in: 0 ..< 3)
     @State private var score: Int = 0
@@ -20,26 +21,32 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            ZStack {
-                VStack {
-                    Text("Score: \(score)")
-                        .frame(alignment: .leading)
-                        .font(.system(size: 30))
-                        .foregroundColor(.orange)
-                    
+            VStack(alignment: .leading) {
+                Text("Score: \(score)")
+                    .frame(alignment: .leading)
+                    .font(.system(size: 30))
+                    .foregroundColor(.orange)
+                
+                VStack(alignment: .center, spacing: 60) {
                     Spacer()
                     
                     Text(shouldWin == .win ? "Choose an option to beat:" : "Choose an option to be defeated by:")
                         .frame(alignment: .center)
                         .font(.system(size: 22))
                         .foregroundColor(.white)
-
-                    Text(moves[moveIndex])
-                        .foregroundColor(shouldWin == .win ? .green : .red)
-                        .frame(alignment: .center)
-                        .font(.system(size: 22, weight: .bold))
-                
-                    Spacer()
+                    
+                    VStack {
+                        Text(emojiHand[moveIndex])
+                            .font(.system(size: 50, weight: .bold))
+                        
+                        Text(moves[moveIndex])
+                            .foregroundColor(.white)
+                            .frame(alignment: .center)
+                            .font(.system(size: 22, weight: .bold))
+                    }
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .background(shouldWin == .win ? Color.green : Color.red)
+                    .cornerRadius(10)
                     
                     HStack {
                         ForEach(0 ..< moves.count) { index in
@@ -47,23 +54,17 @@ struct ContentView: View {
                                 didTapButtonAt(index)
                             }) {
                                 VStack(spacing: 10) {
-                                    if index == 0 {
-                                        Text("👊")
-                                            .font(.system(size: 40))
-                                    } else if index == 1 {
-                                        Text("✋")
-                                            .font(.system(size: 40))
-                                    } else {
-                                        Text("✌️")
-                                            .font(.system(size: 40))
-                                    }
+                                    Text(emojiHand[index])
+                                        .font(.system(size: 40))
                                     Text(moves[index])
+                                        .foregroundColor(Color.white)
                                 }
                             }
                             .frame(width: 100, height: 100, alignment: .center)
                             .background(Color.gray)
                             .font(.system(size: 18, weight: .bold))
                             .foregroundColor(.white)
+                            .border(shouldWin == .win ? Color.red : Color.green, width: 4)
                             .cornerRadius(10)
                             .alert(isPresented: $showingAlert) {
                                 Alert(title: Text("Nice game!"), message: Text("Final score: \(score)"), dismissButton: .default(Text("Okidoki")) {
@@ -73,12 +74,11 @@ struct ContentView: View {
                             }
                         }
                     }
-                    
                     Spacer()
                 }
-                .navigationBarTitle(Text("Challenge: RPC"))
-                
             }
+            .navigationBarTitle(Text("Challenge: RPC"))
+            
         }
         .preferredColorScheme(.dark)
     }
