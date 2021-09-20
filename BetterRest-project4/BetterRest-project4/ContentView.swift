9f29a3eb97fce14c3546d5feea42c4832ddb5219
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2021-09-20 15:51:16 -0300

    challenge: replace each VStack in our form with a Section

--- BetterRest-project4/BetterRest-project4/ContentView.swift
+++ BetterRest-project4/BetterRest-project4/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Form {
-                VStack(alignment: .leading, spacing: 0) {
+                Section {
                     Text("When do you want to wake up?").font(.headline)
                     
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
@@ -27,7 +27,7 @@ struct ContentView: View {
                         .datePickerStyle(WheelDatePickerStyle())
                 }
                 
-                VStack(alignment: .leading, spacing: 0) {
+                Section {
                     Text("Desired amount of sleep").font(.headline)
                     
                     Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
