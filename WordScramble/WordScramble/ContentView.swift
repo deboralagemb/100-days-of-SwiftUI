32f92810e7cc95a158befec3ba3bc4dc8478ab6d
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2021-09-21 14:47:07 -0300

    adding to a list of words

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,13 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     var body: some View {
-        if let fileURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileURL) {
-                print(fileContents.first)
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .autocapitalization(.none)
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
             }
         }
-        return Text("Hello World")
+        .navigationBarTitle(rootWord)
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else {
+            return
+        }
+        
+        usedWords.insert(answer, at: 0)
+        newWord = ""
     }
 }
 
