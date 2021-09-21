e67b57f499af1e05472644826a3d159db1f81af4
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2021-09-21 11:45:58 -0300

    add WordScramble project

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Débora Lage on 21/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            Text("Hello World")
+            Text("Hello World")
+            Text("Hello World")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Débora Lage on 21/09/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
