43956be3798b8695fd2546748c18593a17bb1741
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2021-09-23 14:50:57 -0300

    milestone project started

---
+++ Challenge4-6/Challenge4-6/Challenge4_6App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge4_6App.swift
+//  Challenge4-6
+//
+//  Created by Débora Lage on 23/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Challenge4_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
