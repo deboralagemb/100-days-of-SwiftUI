b88c481427604abaabe6cd07a6a4e41c34d72969
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2024-01-04 22:09:46 -0300

    add: guess the flag project with finished design

---
+++ Project2-GuessTheFlag/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Débora Lage on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
