38634efc1ec5b9ffaa93373a35da8a899b676678
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2024-01-08 22:07:52 -0300

    add: challenge 3 from project 3

--- Project3-ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Project3-ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,15 +7,24 @@
 
 import SwiftUI
 
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundStyle(.tint)
+            .font(.largeTitle)
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Views and Modifiers")
+            .titleStyle()
     }
 }
 
