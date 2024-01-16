ee9ccb342bd327e89706f383494aff40b4781784
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2024-01-16 14:51:41 -0300

    feat: customizing animations

--- Project6-Animations/Animations/Animations/ContentView.swift
+++ Project6-Animations/Animations/Animations/ContentView.swift
@@ -8,17 +8,92 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmountDefault = 1.0
+    @State private var animationAmountCustom1 = 1.0
+    @State private var animationAmountCustom2 = 1.0
+    @State private var animationAmountCustom3 = 1.0
+    @State private var animationAmountCustom4 = 1.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Implicit Animation")
+                .font(.title)
+            
+            HStack {
+                Button("1") {
+                    animationAmountDefault += 1
+                }
+                .padding(30)
+                .background(.red)
+                .foregroundStyle(.white)
+                .clipShape(.circle)
+                .scaleEffect(animationAmountDefault)
+                .blur(radius: (animationAmountDefault - 1) * 3)
+                .animation(.default, value: animationAmountDefault)
+                
+                Button("2") {
+                    animationAmountCustom1 += 1
+                }
+                .padding(30)
+                .background(.red)
+                .foregroundStyle(.white)
+                .clipShape(.circle)
+                .scaleEffect(animationAmountCustom1)
+                .blur(radius: (animationAmountCustom1 - 1) * 3)
+                .animation(.linear, value: animationAmountCustom1)
+                
+                Button("3") {
+                    animationAmountCustom2 += 1
+                }
+                .padding(30)
+                .background(.red)
+                .foregroundStyle(.white)
+                .clipShape(.circle)
+                .scaleEffect(animationAmountCustom2)
+                .blur(radius: (animationAmountCustom2 - 1) * 3)
+                .animation(.spring(duration: 1, bounce: 0.9), value: animationAmountCustom2)
+                
+                Button("4") {
+                    animationAmountCustom3 += 1
+                }
+                .padding(30)
+                .background(.red)
+                .foregroundStyle(.white)
+                .clipShape(.circle)
+                .scaleEffect(animationAmountCustom3)
+                .blur(radius: (animationAmountCustom3 - 1) * 3)
+                .animation(
+                    .easeInOut(duration: 2)
+                        .delay(1)
+                        .repeatCount(3, autoreverses: true),
+                    value: animationAmountCustom3
+                )
+            }
+            
+            HStack {
+                Color.red
+                    .clipShape(.circle)
+                    .frame(width: 70, height: 70)
+                    .overlay(
+                        Circle()
+                            .stroke(.red)
+                            .scaleEffect(animationAmountCustom4)
+                            .opacity(2 - animationAmountCustom4)
+                            .animation(
+                                .easeOut(duration: 1)
+                                    .repeatForever(autoreverses: false),
+                                value: animationAmountCustom4
+                            )
+                    )
+                    .onAppear {
+                        animationAmountCustom4 = 2
+                    }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
