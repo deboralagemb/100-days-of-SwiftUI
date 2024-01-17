f7b777c75c0126931ee58402c0206a35b78799a4
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2024-01-16 21:10:42 -0300

    add: navigation for animations

--- Project6-Animations/Animations/Animations/ContentView.swift
+++ Project6-Animations/Animations/Animations/ContentView.swift
@@ -7,120 +7,204 @@
 
 import SwiftUI
 
+enum Animations: String, CaseIterable {
+    case implicitAnimation1
+    case implicitAnimation2
+    case implicitAnimation3
+    case implicitAnimation4
+    case implicitAnimation5
+    case animatingBindings
+    case explicitAnimation1
+}
+
 struct ContentView: View {
-    @State private var animationAmountDefault = 1.0
-    @State private var animationAmountCustom1 = 1.0
-    @State private var animationAmountCustom2 = 1.0
-    @State private var animationAmountCustom3 = 1.0
-    @State private var animationAmountCustom4 = 1.0
-    @State private var animationBindings = 1.0
-    @State private var animationAmountExplicit = 0.0
-    
+    @State private var animations = [
+        "Implicit Animation 1",
+        "Implicit Animation 2",
+        "Implicit Animation 3",
+        "Implicit Animation 4",
+        "Implicit Animation 5",
+        "Animating Bindings",
+        "Explicit Animation 1"
+    ]
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(Array(animations.enumerated()), id: \.offset) { index, animation in
+                    NavigationLink(destination: DetailView(animationTitle: animation, animationType: Animations.allCases[index])) {
+                        Line(text: animation)
+                    }
+                }
+            }
+            .navigationTitle("Animations")
+        }
+    }
+}
+
+struct DetailView: View {
+    var animationTitle: String
+    var animationType: Animations
+
     var body: some View {
         VStack {
-            Text("Implicit Animation")
-                .font(.title)
-            
-            HStack {
-                Button("1") {
-                    animationAmountDefault += 1
-                }
-                .padding(30)
-                .background(.red)
-                .foregroundStyle(.white)
-                .clipShape(.circle)
-                .scaleEffect(animationAmountDefault)
-                .blur(radius: (animationAmountDefault - 1) * 3)
-                .animation(.default, value: animationAmountDefault)
-                
-                Button("2") {
-                    animationAmountCustom1 += 1
-                }
-                .padding(30)
-                .background(.red)
-                .foregroundStyle(.white)
-                .clipShape(.circle)
-                .scaleEffect(animationAmountCustom1)
-                .blur(radius: (animationAmountCustom1 - 1) * 3)
-                .animation(.linear, value: animationAmountCustom1)
-                
-                Button("3") {
-                    animationAmountCustom2 += 1
-                }
-                .padding(30)
-                .background(.red)
-                .foregroundStyle(.white)
-                .clipShape(.circle)
-                .scaleEffect(animationAmountCustom2)
-                .blur(radius: (animationAmountCustom2 - 1) * 3)
-                .animation(.spring(duration: 1, bounce: 0.9), value: animationAmountCustom2)
-                
-                Button("4") {
-                    animationAmountCustom3 += 1
-                }
-                .padding(30)
-                .background(.red)
-                .foregroundStyle(.white)
-                .clipShape(.circle)
-                .scaleEffect(animationAmountCustom3)
-                .blur(radius: (animationAmountCustom3 - 1) * 3)
-                .animation(
-                    .easeInOut(duration: 2)
-                        .delay(1)
-                        .repeatCount(3, autoreverses: true),
-                    value: animationAmountCustom3
-                )
+            switch animationType {
+            case .implicitAnimation1:
+                ImplicitAnimation1()
+            case .implicitAnimation2:
+                ImplicitAnimation2()
+            case .implicitAnimation3:
+                ImplicitAnimation3()
+            case .implicitAnimation4:
+                ImplicitAnimation4()
+            case .implicitAnimation5:
+                ImplicitAnimation5()
+            case .animatingBindings:
+                AnimatingBindings()
+            case .explicitAnimation1:
+                ExplicitAnimations()
             }
-            
-            HStack {
-                Color.red
-                    .clipShape(.circle)
-                    .frame(width: 70, height: 70)
-                    .overlay(
-                        Circle()
-                            .stroke(.red)
-                            .scaleEffect(animationAmountCustom4)
-                            .opacity(2 - animationAmountCustom4)
-                            .animation(
-                                .easeOut(duration: 1)
-                                    .repeatForever(autoreverses: false),
-                                value: animationAmountCustom4
-                            )
-                    )
-                    .onAppear {
-                        animationAmountCustom4 = 2
-                    }
-            }
-            
-            Text("Animating Bindings")
-                .font(.title)
-            
-            HStack {
-                Stepper("Scale amount", value: $animationBindings.animation(), in: 1...10)
-                Button("6") {
-                    animationBindings += 1
-                }
-                .padding(40)
-                .background(.red)
-                .foregroundStyle(.white)
-                .clipShape(.circle)
-                .scaleEffect(animationBindings)
-            }
-            
-            Text("Explicit Animations")
-                .font(.title)
-
-            Button("7") {
-                withAnimation(.spring(duration: 1, bounce: 0.5)) {
-                    animationAmountExplicit += 360
-                }
-            }
-            .padding(30)
-            .background(.red)
-            .foregroundStyle(.white)
-            .clipShape(.circle)
-            .rotation3DEffect(.degrees(animationAmountExplicit), axis: (x: 0, y: 1, z: 0))
         }
+        .navigationTitle(animationTitle)
+    }
+}
+
+struct Line: View {
+    var text: String
+
+    var body: some View {
+        Text(text)
+    }
+}
+
+struct ImplicitAnimation1: View {
+    @State private var animationAmount = 1.0
+    
+    var body: some View {
+        Button("Tap me") {
+            animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(.default, value: animationAmount)
+    }
+}
+
+struct ImplicitAnimation2: View {
+    @State private var animationAmount = 1.0
+    
+    var body: some View {
+        Button("Tap me") {
+            animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(.linear, value: animationAmount)
+    }
+}
+
+struct ImplicitAnimation3: View {
+    @State private var animationAmount = 1.0
+    
+    var body: some View {
+        Button("Tap me") {
+            animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(.spring(duration: 1, bounce: 0.9), value: animationAmount)
+    }
+}
+
+struct ImplicitAnimation4: View {
+    @State private var animationAmount = 1.0
+    
+    var body: some View {
+        Button("Tap me") {
+            animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(
+            .easeInOut(duration: 2)
+            .delay(1)
+            .repeatCount(3, autoreverses: true),
+            value: animationAmount
+        )
+    }
+}
+
+struct ImplicitAnimation5: View {
+    @State private var animationAmount = 1.0
+    
+    var body: some View {
+        Color.red
+            .clipShape(.circle)
+            .padding(50)
+            .overlay(
+                Circle()
+                    .stroke(.red)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+                    .animation(
+                        .easeOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                        value: animationAmount
+                    )
+            )
+            .onAppear {
+                animationAmount = 2
+            }
+    }
+}
+
+struct AnimatingBindings: View {
+    @State private var animationAmount = 1.0
+    
+    var body: some View {
+        Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+        Spacer()
+        Button("Tap me") {
+            animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .scaleEffect(animationAmount)
+    }
+}
+
+struct ExplicitAnimations: View {
+    @State private var animationAmount = 0.0
+    
+    var body: some View {
+        Button("Tap me") {
+            withAnimation(.spring(duration: 1, bounce: 0.5)) {
+                animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
     }
 }
 
