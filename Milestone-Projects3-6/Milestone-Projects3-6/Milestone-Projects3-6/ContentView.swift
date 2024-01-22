551eb477089320e4e221118647f0a07b87770fa3
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2024-01-22 11:30:48 -0300

    add: colors to milestone project

--- Milestone-Projects3-6/Milestone-Projects3-6/Milestone-Projects3-6/ContentView.swift
+++ Milestone-Projects3-6/Milestone-Projects3-6/Milestone-Projects3-6/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct Question {
     var text: String
-    var answer: String
+    var answer: Int
 }
 
 struct ContentView: View {
@@ -23,9 +23,11 @@ struct ContentView: View {
     
     @State var numberOfQuestions: Int = 5
     @State var showingEndOfGame: Bool = false
+    @State var userGuess: Int = 0
+    @State var score: Int = 0
 
     init() {
-        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: #colorLiteral(red: 0, green: 0, blue: 0.3803921569, alpha: 0.8470588235)]
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)]
     }
     
     var body: some View {
@@ -36,21 +38,51 @@ struct ContentView: View {
                 
                 if gameIsActive {
                     VStack {
-                        Text(questions[currentQuestion].text)
-                        Text(questions[currentQuestion].answer)
-                        Button {
-                            if currentQuestion == numberOfQuestions - 1 {
-                                showingEndOfGame = true
-                            } else {
-                                currentQuestion += 1
+                        ZStack {
+                            HStack {
+                                Spacer(minLength: 30)
+                                Color.blue
+                                    .frame(height: 150)
+                                    .clipShape(.buttonBorder)
+                                    .shadow(radius: 5)
+                                Spacer(minLength: 30)
                             }
-                        } label: {
-                            Text("next question")
+                            
+                            Text(questions[currentQuestion].text)
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .foregroundStyle(.white)
+                        }
+
+                        HStack {
+                            Spacer()
+                            TextField("Value",
+                                      value: $userGuess,
+                                      format: .number)
+                            .textFieldStyle(.roundedBorder)
+                            .keyboardType(.decimalPad)
+                            .toolbar {
+                                ToolbarItem(placement: .keyboard) {
+                                    Button("Done") {
+                                        if userGuess == questions[currentQuestion].answer {
+                                            score += 1
+                                        }
+                                        
+                                        if currentQuestion == numberOfQuestions - 1 {
+                                            showingEndOfGame = true
+                                        } else {
+                                            currentQuestion += 1
+                                        }
+                                    }
+                                }
+                            }
+                            Spacer()
                         }
                     }
                 } else {
                     VStack {
                         VStack(spacing: 20) {
+
                             VStack {
                                 Text("Select the multiplication table you want to practice")
                                     .font(.headline)
@@ -65,7 +97,7 @@ struct ContentView: View {
                             .padding(20)
                             .background(.ultraThinMaterial)
                             .clipShape(.buttonBorder)
-                            
+
                             VStack {
                                 Text("How many questions do you want to be asked?")
                                     .font(.headline)
@@ -89,9 +121,10 @@ struct ContentView: View {
                             } label: {
                                 Text("Start game!")
                                     .font(.headline)
+                                    .foregroundStyle(.black)
                             }
                             .padding(20)
-                            .background(.yellow)
+                            .background(Color("secondary_pink"))
                             .clipShape(.capsule)
                         }
                         .padding()
@@ -102,7 +135,7 @@ struct ContentView: View {
             .alert("End of game", isPresented: $showingEndOfGame) {
                 Button("Play again", action: resetGame)
             } message: {
-                Text("Your game total score was: score")
+                Text("Your game total score was: \(score)")
             }
         }
     }
@@ -111,7 +144,7 @@ struct ContentView: View {
         let range = 0..<numberOfQuestions
         range.forEach { index in
             let randomNumber = Int.random(in: 0..<12)
-            let question = Question(text: "What is \(randomNumber) x \(numberSelected)", answer: "\(numberSelected*randomNumber)")
+            let question = Question(text: "\(randomNumber) x \(numberSelected)", answer: numberSelected*randomNumber)
             
             questions.append(question)
         }
@@ -130,9 +163,7 @@ struct ContentView: View {
 struct BackgroundView: View {
     
     var body: some View {
-        LinearGradient(colors: [.yellow, .blue],
-                       startPoint: .top,
-                       endPoint: .bottom)
+        Color("primary_yellow")
             .ignoresSafeArea()
     }
 }
