e9f2ef50f44f1c95c92c97c10aa2cad5df8d606d
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2024-01-25 20:52:28 -0300

    add: NavigationStack to project 9

---
+++ Project9-Navigation/Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Débora Lage on 24/01/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
