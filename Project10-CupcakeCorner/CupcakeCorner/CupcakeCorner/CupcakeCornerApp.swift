05e4876f989b73171e43761a513b8b096e21ac77
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2024-01-31 14:33:41 -0300

    add: project 10

---
+++ Project10-CupcakeCorner/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Débora Lage on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
