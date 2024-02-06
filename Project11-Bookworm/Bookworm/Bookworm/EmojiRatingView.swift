1af696d764c3353387fc25b4d2b5f172348ee31c
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2024-02-06 15:06:09 -0300

    add: list with @Query

---
+++ Project11-Bookworm/Bookworm/Bookworm/EmojiRatingView.swift
@@ -0,0 +1,36 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Débora Lage on 06/02/24.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int
+    
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            Text("☹️")
+            
+        case 2:
+            Text("🥱")
+            
+        case 3:
+            Text("😑")
+            
+        case 4:
+            Text("😊")
+            
+        default:
+            Text("🤯")
+        }
+    }
+}
+
+#Preview {
+    EmojiRatingView(rating: 3)
+}
