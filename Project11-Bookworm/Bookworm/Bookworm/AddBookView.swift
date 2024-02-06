49fe785441bb54ab3e64d565aa21a24d560d5194
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2024-02-06 18:37:41 -0300

    add: challenge 1 for project 11

--- Project11-Bookworm/Bookworm/Bookworm/AddBookView.swift
+++ Project11-Bookworm/Bookworm/Bookworm/AddBookView.swift
@@ -14,10 +14,10 @@ struct AddBookView: View {
     @State private var title = ""
     @State private var author = ""
     @State private var rating = 3
-    @State private var genre = "Fantasy"
+    @State private var genre = "Unknown"
     @State private var review = ""
     
-    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller", "Unknown"]
     var isValidBook: Bool {
         if title.isOnlySpaces || author.isOnlySpaces || review.isOnlySpaces {
             return false
