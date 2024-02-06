d419ce07668de29a1d1baf0d26fdf9bec90d52a5
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2024-02-06 10:25:37 -0300

    add: creating books with swiftdata

---
+++ Project11-Bookworm/Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Débora Lage on 05/02/24.
+//
+
+import SwiftData
+import SwiftUI
+
+struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var books: [Book]
+    
+    @State private var showingAddScreen = false
+    
+    var body: some View {
+        NavigationStack {
+            Text("Count: \(books.count)")
+                .navigationTitle("Bookworm")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Add Book", systemImage: "plus") {
+                            showingAddScreen.toggle()
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView()
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
